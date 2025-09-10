6a40b33d23a98392901e3233f5e9fb931afa8969
chantro <sophia2359@gmail.com>
2025-09-10 15:50:18 +0000

    modify json format

--- Endpoint_Infra/modules/alarms/variables.tf
+++ Endpoint_Infra/modules/alarms/variables.tf
@@ -1,5 +1,14 @@
 variable "endpoint_name" { type = string }
-variable "variant_name"  { type = string  default = "AllTraffic" }
-variable "sns_topic_arn" { type = string  default = null }
-variable "latency_p95_threshold_ms" { type = number default = 500 }
+variable "variant_name"  { 
+  type = string  
+  default = "AllTraffic" 
+}
+variable "sns_topic_arn" { 
+  type = string  
+  default = null 
+}
+variable "latency_p95_threshold_ms" { 
+  type = number 
+  default = 500 
+}
 
--- Endpoint_Infra/variables.tf
+++ Endpoint_Infra/variables.tf
@@ -1,8 +1,23 @@
-variable "model_package_group" { type = string }
-variable "github_owner"        { type = string }
-variable "github_repo"         { type = string }
-variable "github_pat_secret_arn" { type = string }
-variable "ops_sns_topic_arn"   { type = string  default = null } 
-variable "sagemaker_processing_role_arn" { type = string }
-variable "monitor_image_uri"             { type = string } # 예:  <acct>.dkr.ecr.<region>.amazonaws.com/sagemaker-model-monitor-analyzer
+variable "model_package_group" { 
+  type = string 
+}
+variable "github_owner"        { 
+  type = string 
+}
+variable "github_repo"         { 
+  type = string 
+}
+variable "github_pat_secret_arn" { 
+  type = string 
+}
+variable "ops_sns_topic_arn"   { 
+  type = string  
+  default = null
+} 
+variable "sagemaker_processing_role_arn" { 
+  type = string 
+}
+variable "monitor_image_uri"             { 
+  type = string 
+} # 예:  <acct>.dkr.ecr.<region>.amazonaws.com/sagemaker-model-monitor-analyzer
 
