7d3c3c95537f3dd674b267d6f319edd496b90c01
chantro <sophia2359@gmail.com>
2025-09-10 04:13:32 +0000

    .....

--- pipeline/Infra/pipeline/sagemaker_pipeline.tf
+++ pipeline/Infra/pipeline/sagemaker_pipeline.tf
@@ -1,8 +1,14 @@
+locals {
+  pipeline_def_json = (
+    var.pipeline_definition_json != null && trim(var.pipeline_definition_json) != ""
+  ) ? var.pipeline_definition_json : "{}"
+}
+
 resource "aws_sagemaker_pipeline" "fraud_pipenline" {
 	pipeline_name = "FraudDetectionPipeline"
 	pipeline_display_name = "Fraud-Detection-Pipeline"
 	role_arn = var.sagemaker_role_arn
-	pipeline_definition   = var.pipeline_definition_json
+	pipeline_definition   = local.pipeline_def_json 
 	tags={
 		project = "fraud-detecion"
 	}
