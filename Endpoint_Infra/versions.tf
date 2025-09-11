bc9facbb8040994061cdffd08f903f2cdb6f8e4f
chantro <sophia2359@gmail.com>
2025-09-11 02:00:20 +0000

    upgrade terrafirm version

--- Endpoint_Infra/versions.tf
+++ Endpoint_Infra/versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.60"    # 5.6x대 권장 (팀 표준에 맞게 조정 가능)
+      version = "~> 5.66"    # 5.6x
     }
   }
 }
